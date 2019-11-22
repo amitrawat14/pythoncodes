6da36a529dea1ea997b8709ae9a6a39033d075c6
amitrawat14 <33389459+amitrawat14@users.noreply.github.com>
2019-11-22 18:29:34 +0530

    Update Find_Duplicate_Lines.py

--- Find_Duplicate_Lines.py
+++ Find_Duplicate_Lines.py
@@ -1,6 +1,8 @@
 ## Script find the unique lines in the file, i.e We have network flows and we need uniques flows source/destination/port in thousands 
 ## of flows . 
 
+## Script find the unique lines in the file, 
+
 import csv
 reader = csv.reader(open("log.csv"))
 reader = list(reader)
@@ -13,4 +15,16 @@ for line in reader[1:]:
 
 print("Tola line scanned" , len(reader))
 print("Total Unique line found",len(duplicate))
+
+---
+
+outputFile = open('output.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+for index,line in enumerate(duplicate):
+    print(index,line)
+    outputWriter.writerow(line)
+
+outputFile.close()
+
+
 
